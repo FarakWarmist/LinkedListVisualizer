c434ef6373c30b4028b81044514081d9b81eb601
William Sylvestre <farakwarts@gmail.com>
2025-02-19 17:12:08 -0500

    Press "q" to add a node to the head of the list

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,8 +1,10 @@
 #include "ofApp.h"
+#include <cstdlib>
+#include <ctime>
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+	circlesList.insertAtHead(10 + std::rand() % 91);
 }
 
 //--------------------------------------------------------------
@@ -12,12 +14,34 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+	Node* temp = circlesList.head;
+	Node* prev = nullptr;
+	index = 0;
 
+	while (temp != nullptr) {
+		ofSetColor(255);
+		ofDrawCircle(100 + index * 200, 200, temp->data);
+		ofSetColor(0);
+		ofDrawBitmapString(ofToString(temp->data), (100 + index * 200) - 5, 200 + 5);
+
+		if (prev != nullptr)
+		{
+			ofSetColor(255);
+			ofDrawLine(100 + (index - 1) * 200, 200, 100 + index * 200, 200);
+		}
+
+		prev = temp;
+		temp = temp->next;
+		index++;
+	}
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+	if (key == 'q')
+	{
+		circlesList.insertAtHead(10 + std::rand() % 91);
+	}
 }
 
 //--------------------------------------------------------------
